8f04095a5110e227a765509d6288f5f740f6ffd0
mmbaesso1980 <mmbaesso@hotmail.com>
2026-03-27 10:46:32 -0300

    feat: add GithubAuthProvider to firebase config

--- app/src/lib/firebase.js
+++ app/src/lib/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, GithubAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getFunctions } from "firebase/functions";
 
@@ -15,5 +15,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
+export const githubProvider = new GithubAuthProvider();
 export const db = getFirestore(app);
 export const functions = getFunctions(app, "southamerica-east1");
